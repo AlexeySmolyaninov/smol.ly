2901de518cf624be71997720a18ed8f0db9af127
alexey.smolyaninov <alexey.smolyaninov@op.fi>
2022-02-14 01:32:27 +0200

    smol.ly#19: added prettier + vscode settings + formated files

--- service/src/app.ts
+++ service/src/app.ts
@@ -2,5 +2,5 @@ import express from 'express';
 
 export const app = express();
 
-app.get("/", (_req, res) => res.redirect("/api/health"))
+app.get('/', (_req, res) => res.redirect('/api/health'));
 app.get('/api/health', (_req, res) => res.send({ status: 'UP' }));
