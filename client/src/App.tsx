c32746cd217fa597f9c5bdf6d8b166a5136284ab
alexey.smolyaninov <alexey.smolyaninov@op.fi>
2022-02-14 23:20:08 +0200

    smol.ly#20: added eslinter for client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,5 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Smol.ly</h1>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => (
+  <div className="App">
+    <h1>Smol.ly</h1>
+  </div>
+);
